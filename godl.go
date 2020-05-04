ee0c07f25490c62244f3c13acd1261769451609a
dqn <suyarizzz@gmail.com>
2020-05-04 23:29:16 +0900

    feat: show progress

--- godl.go
+++ godl.go
@@ -1,25 +1,60 @@
 package godl
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func Download(rawURL, dest string) error {
+const (
+	mb     = 1 << 20
+	format = "%.2f"
+)
+
+type progress struct {
+	numerator   int64
+	denominator int64
+}
+
+func (p *progress) Write(b []byte) (int, error) {
+	n := len(b)
+	p.numerator += int64(n)
+
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+	fmt.Printf("\rdownloading... "+format, float64(p.numerator)/mb)
+
+	if p.denominator > 0 {
+		fmt.Printf("/"+format, float64(p.denominator)/mb)
+	}
+
+	fmt.Print(" MB")
+
+	return n, nil
+}
+
+func Download(rawURL, dest string, showProgress bool) error {
 	resp, err := http.Get(rawURL)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	file, err := os.Create(dest)
+	f, err := os.Create(dest)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer f.Close()
 
-	_, err = io.Copy(file, resp.Body)
+	if showProgress {
+		w := &progress{denominator: resp.ContentLength}
+		_, err = io.Copy(f, io.TeeReader(resp.Body, w))
+		fmt.Println()
+	} else {
+		_, err = io.Copy(f, resp.Body)
+	}
+
 	if err != nil {
 		return err
 	}
